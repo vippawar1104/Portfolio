f063da98c9886ab8c366f0dd0f4791c105f0d2c7
Vipul Pawar <vipulpawar81077@gmail.com>
2025-05-30 14:07:10 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com'],
-  }
-}
+import type { NextConfig } from "next";
 
-export default nextConfig
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
